eb9020b31a83b0f72105a7c6ca4755b5433cef65
ariefdarmawan <ariefdarmawan@ECLTADA02.local>
2017-10-03 09:59:35 +0800

    update

---
+++ test/sqlh_test.go
@@ -0,0 +1,86 @@
+package test
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/eaciit/toolkit"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	"github.com/eaciit/sqlh"
+)
+
+const (
+	sqlconn = "root:Database.1024@tcp(localhost:3306)/ectestdb"
+)
+
+var (
+	db  *sql.DB
+	err error
+)
+
+type Employee struct {
+	ID   string
+	Name string
+}
+
+func TestConnect(t *testing.T) {
+	db, err = sqlh.Connect("mysql", sqlconn)
+	if err != nil {
+		fmt.Println("error connecting database", err)
+		os.Exit(100)
+	}
+}
+
+func TestCreateTable(t *testing.T) {
+	sql := "CREATE TABLE test_table_model2(\n" +
+		"	id      	VARCHAR(32)     NOT NULL,\n" +
+		"	name  		VARCHAR(200)    NOT NULL,\n" +
+		"	level  		INT    			NOT NULL,\n" +
+		"	datejoin   	DATE   			NOT NULL,\n" +
+		"	PRIMARY KEY (id)\n" +
+		");\n"
+
+	qr := sqlh.Exec(db, sqlh.ExecNonScalar, sql)
+	if qr.Error() != nil {
+		t.Error(qr.Error())
+	}
+}
+
+func TestInsert(t *testing.T) {
+	sql := "insert into test_table_model2 (id, name, level, datejoin) values(?,?,?,?)"
+	id := toolkit.RandomString(32)
+	name := "Name " + id
+	qr := sqlh.Exec(db, sqlh.ExecNonScalar, sql, id, name, 100, "2017-01-01")
+	if qr.Error() != nil {
+		t.Error(qr.Error())
+	} else {
+		affected, _ := qr.CUDAResult().RowsAffected()
+		fmt.Println("Data inserted: ", affected)
+	}
+}
+
+func TestSelect(t *testing.T) {
+	sql := "select * from test_table_model2"
+	es := []Employee{}
+
+	qr := sqlh.Exec(db, sqlh.ExecQuery, sql)
+	if qr.Error() != nil {
+		t.Error(qr.Error())
+	}
+
+	err = qr.Fetch(&es, 0)
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Printf("Returned record:%d\n%s", len(es), toolkit.JsonString(es))
+}
+
+func TestClose(t *testing.T) {
+	if db != nil {
+		db.Close()
+	}
+}
